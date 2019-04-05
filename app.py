80f6985d4de7a2f6858a9d72137be53bf35382b7
Lucas <suarez.luks@gmail.com>
2019-04-05 13:35:53 +0000

    Add recipes form created and succesfully connected to Mongodb

--- app.py
+++ app.py
@@ -14,7 +14,17 @@ mongo = PyMongo(app)
 @app.route('/get_recipes')
 def get_recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html',
+                           categories=mongo.db.categories.find())
 
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
