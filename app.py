77ec20a97fefa4aee649ab02b8bc47b56627a066
Lucas <suarez.luks@gmail.com>
2019-04-06 09:20:20 +0000

    Edit recipes funcionality added and page created. Also some css styles have been applied

--- app.py
+++ app.py
@@ -25,6 +25,29 @@ def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
+    
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           categories=all_categories)
+
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name':request.form.get('recipe_name'),
+        'category_name':request.form.get('category_name'),
+        'ingredients': request.form.get('ingredients'),
+        'description': request.form.get('description'),
+        'preparation_time': request.form.get('preparation_time'),
+        'photo_url': request.form.get('photo_url')
+        
+    })
+    return redirect(url_for('get_recipes'))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
