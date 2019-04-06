ad5a4ed3b25d028d3d18cb84a6efe129a5d91d8c
Lucas <suarez.luks@gmail.com>
2019-04-06 20:08:26 +0000

    Delete functionality added

--- app.py
+++ app.py
@@ -49,6 +49,11 @@ def update_recipe(recipe_id):
     })
     return redirect(url_for('get_recipes'))
 
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
